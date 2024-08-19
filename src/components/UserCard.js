6e155b68291cf299cea06e4e27df82df07a19cae
shantal rhobi <shantal.rohbi@student.moringaschool.com>
2024-08-19 23:14:42 +0300

    complete lab

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,9 +1,10 @@
-
+import { Link } from "react-router-dom";
 
 function UserCard({user}) {
   return (
     <article>
         <h2>{user.name}</h2>
+        <Link to={` ${user.id}`}>View Profile</Link>
     </article>
   );
 };
